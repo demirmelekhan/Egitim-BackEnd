010af2de05d76008d3e1531d4fcc020927bae277
Melek Demirhan <149537896+demirmelekhan@users.noreply.github.com>
2024-12-28 15:12:03 +0300

    ucgen_cizme proje klasörü eklendi.

---
+++ dotnetCORE/Project/ucgen_cizme/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ucgen_cizme
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CizUcgen();
+        }
+
+        public static void CizUcgen()
+        {
+            Console.WriteLine("Üçgenin boyutunu girin: ");
+            if (int.TryParse(Console.ReadLine(), out int boyut) && boyut > 0)
+            {
+                for (int i = 1; i <= boyut; i++)
+                {
+                    // Boşlukları yazdır
+                    for (int j = 0; j < boyut - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    // Yıldızları yazdır
+                    for (int k = 0; k < i; k++)
+                    {
+                        Console.Write("* ");
+                    }
+                    // Yeni satıra geç
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Lütfen geçerli bir pozitif tam sayı girin.");
+            }
+        }
+    }
+}
