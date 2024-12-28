c141b3e47ba34ba1635cbaccd557e972b5626908
Melek Demirhan <149537896+demirmelekhan@users.noreply.github.com>
2024-12-28 10:53:04 +0300

    Project klasörü eklendi / fibonacci_ort_hesaplama proje dosyayı gerçekleştirildi.

---
+++ dotnetCORE/Project/fibonacci_ort_hesaplama/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fibonacci_ort_hesaplama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci serisinin kaç elemanının ortalamasını istediğinizi yazın: ");
+            int rakam = Convert.ToInt32(Console.ReadLine());
+
+            double ortalama = FibonacciOrt(rakam);
+            Console.WriteLine($"Ortalaması: {ortalama}");
+            
+        }
+
+        public static double FibonacciOrt(int deger)
+        {
+            if(deger<=0) return 0;
+
+            int first=1;
+            int second=1;
+            int toplam= first + second;
+
+            for (int i = 3; i <= deger ; i++)
+            {
+                int twin=first + second;
+                toplam +=twin;
+                first=second;
+                second=twin;
+                
+            }
+            return (double)toplam/deger;
+        }
+    }
+}
