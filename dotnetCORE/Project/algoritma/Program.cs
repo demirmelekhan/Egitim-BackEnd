64c909e0ffcba5325363d8347d1da1caa11fe8aa
Melek Demirhan <149537896+demirmelekhan@users.noreply.github.com>
2025-04-28 00:37:29 +0300

    Algoritma isimli proje eklendi

---
+++ dotnetCORE/Project/algoritma/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace algoritma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir kelime girin virgülden sonra da bir sayı:");
+            string input = Console.ReadLine();
+            string [] kelimeler= input.Split(',');
+            string newinput=kelimeler[0].Remove(Convert.ToInt32(kelimeler[1]),1);
+            Console.WriteLine("Yeni çıktı: "+ newinput);
+
+        }
+    }
+}
