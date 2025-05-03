f67455a74f41a6c4b55a162b450fc8647eeb4a9c
Melek Demirhan <149537896+demirmelekhan@users.noreply.github.com>
2025-05-03 16:40:33 +0300

    input_reverse proje dosyası eklendi

---
+++ dotnetCORE/Project/input_reverse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace input_reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Bir cümle girin: ");
+            string input = Console.ReadLine();
+            char[] karakterler =input.ToCharArray();
+            Array.Reverse(karakterler);
+            string ters = new string(karakterler);
+
+            Console.WriteLine(ters);
+        }
+    }
+}
