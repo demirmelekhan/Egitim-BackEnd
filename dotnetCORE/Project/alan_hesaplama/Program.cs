d096ffb909e41f7452ad307240c4d711072c659c
Melek Demirhan <149537896+demirmelekhan@users.noreply.github.com>
2025-05-07 14:05:41 +0300

    Alan hesaplama proje dosyası eklendi.

---
+++ dotnetCORE/Project/alan_hesaplama/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace alan_hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Hangi şekil için alan hesabı yapmak istediğinizi seçin\n 1-KARE\n 2-DİKDÖRTGEN\n 3-DAİRE\n 4-ÜÇGEN");
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 1: KareAlan();
+                            break;
+                    case 2: DikdortgenAlan();
+                            break;
+                    case 3: DaireAlan();
+                            break;
+                    case 4: UcgenAlan();
+                            break;
+                    case 0:
+                        Console.WriteLine("Programdan çıkılıyor....");
+                        return;
+                    default:
+                        Console.WriteLine("Yanlış giriş yaptınız seçeneklerden birini ifade eden rakamsal karşılığı girin...");
+                        break;
+                }
+            }
+            Console.WriteLine(); // Görsel olarak bir boşluk bırak
+        }
+        static void KareAlan()
+        {   
+            Console.WriteLine("Kenar uzunluğunu giriniz:");
+            int deger =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girilen Kare cisminin Alanı:"+ Math.Pow(deger,2));
+        }
+        static void DikdortgenAlan()
+        {
+            Console.WriteLine("Dikdörtgene ait iki komşu kenarın uzunluklarını giriniz:");
+            int deger1 =Convert.ToInt32(Console.ReadLine());
+            int deger2 =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girilen Dikdörtgen cisminin Alanı:"+deger1*deger2);
+        }
+        static void UcgenAlan()
+        {
+            Console.WriteLine("Taban uzunluğunu giriniz:");
+            int taban =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Yükseklik bilgilerini giriniz:");
+            int yukseklik =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girilen Üçgen cisminin Alanı: "+ (taban*yukseklik)/2);
+        }
+        static void DaireAlan()
+        {
+            const double pi = 3.14159265;
+            Console.WriteLine("Yarıçap uzunluğunu giriniz:");
+            double deger =Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Girilen Daire cisminin Alanı:"+ Math.Pow(deger,2)*pi);
+        }
+    }
+}
--- dotnetCORE/Project/alan_hesaplama/obj/Debug/net8.0/alan_hesaplama.AssemblyInfo.cs
+++ dotnetCORE/Project/alan_hesaplama/obj/Debug/net8.0/alan_hesaplama.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("arayuz")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("alan_hesaplama")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f67455a74f41a6c4b55a162b450fc8647eeb4a9c")]
-[assembly: System.Reflection.AssemblyProductAttribute("arayuz")]
-[assembly: System.Reflection.AssemblyTitleAttribute("arayuz")]
+[assembly: System.Reflection.AssemblyProductAttribute("alan_hesaplama")]
+[assembly: System.Reflection.AssemblyTitleAttribute("alan_hesaplama")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
